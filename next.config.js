1f778f4f58eae78be6cd66a9daff100b56414d25
Tanguy <143474900+TanguyGrd@users.noreply.github.com>
2026-01-10 23:56:12 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['ae-pic-a1.aliexpress-media.com', 'ae01.alicdn.com'],
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
